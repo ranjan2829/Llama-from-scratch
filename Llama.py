0e93cbd1946a0d9715b3992d4ec9bedc6d5a1b19
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-08-15 00:31:16 +0530

    back in game

--- Llama.py
+++ Llama.py
@@ -135,7 +135,7 @@ optimizer=torch.optim.Adam(
     model.parameters(),
 )
 
-def train(model,optimizer,scheduler=None,config=MASTER_CONFIG,print_logs=False):
+def train(model,optimizer,scheduler=None,config=MASTER_CONFIG,print_logs=True):
     losses=[]
     start_time=time.time()
     for epoch in range(config['epochs']):
